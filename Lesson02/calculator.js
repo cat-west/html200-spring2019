e24b5371bc8d82ce64d3d7d2325a0108d673f2bc
Cwest <46390605+caitlinwest10@users.noreply.github.com>
2019-04-10 20:48:55 -0500

    Adding calculator file

---
+++ Lesson02/calculator.js
@@ -0,0 +1,25 @@
+let q = 25;
+let r = 10;
+console.log(q % r);
+
+let i = 8;
+let j = 6;
+let k = 4;
+console.log(i / (j - k));
+
+let g = 3;
+let h = 1;
+console.log(g**(h--));
+
+let x = 6;
+let y = 4;
+console.log(x - y); 
+
+let a = 3;
+let b = 2;
+let c =1;
+console.log(a - (b * c));
+
+let e = -1;
+let f = 1;
+console.log(e + f);
