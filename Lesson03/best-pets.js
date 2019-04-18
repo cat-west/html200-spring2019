1d12ffa735499573570c9c302a5125bc9d81e2d4
Cwest <46390605+caitlinwest10@users.noreply.github.com>
2019-04-17 19:10:36 -0500

    Adding Lesson 03 Assignment 1

---
+++ Lesson03/best-pets.js
@@ -0,0 +1,14 @@
+let bestPets = ['cats', 'dogs', 'budgies', 'snakes', 'rabbits']; {
+    console.log(bestPets)
+ }
+    bestPets.push('guinea pigs'); {
+       console.log(bestPets);
+    }
+ 
+    bestPets.splice(2, 1); {
+       console.log(bestPets);
+    }
+    
+   let petStore = bestPets.join(); {
+      console.log(petStore);
+   }
